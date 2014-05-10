2cf17e01d8e6835f9c76e94bea039c2d82ecd259
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2014-05-10 13:53:43 -0500

    reverse chars in stack for more comprehensible output

--- stack/stack.go
+++ stack/stack.go
@@ -17,6 +17,14 @@ func (s Stack) Peek() byte {
 	return s[len(s)-1]
 }
 
+func reverse(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
 func (s Stack) String() string {
-	return "Stack «" + string(s) + "»"
+	return "Stack «" + reverse(string(s)) + "»"
 }
